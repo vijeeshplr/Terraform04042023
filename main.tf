5db35a2a07ad81ab262cd211cd540c295fe8fd54
vijeeshplr <vijeesh.plr@gmail.com>
2023-04-08 21:22:21 -0700

    new chnages in terraform.yaml

--- main.tf
+++ main.tf
@@ -3,7 +3,16 @@ provider "aws" {
   region     = "us-east-1" # Replace with your desired region
 }
 
-
+terraform {
+  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
+  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
 module "elasticdev"{
  source = "./EC2"
@@ -14,4 +23,14 @@ module "tf-state" {
     source = "./CI-CD/tf-state"
     bucket_name  = "cc-tf-state-backend-ci-cd"
   
+}
+
+module "vpc-infra" {
+  source = "./modules/vpc"
+
+  # VPC Input Vars
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
 }
