7e31d54024036bfd5b0dbe9af2759b7842b92d84
vijeeshplr <vijeesh.plr@gmail.com>
2023-04-08 23:02:29 -0700

    new

--- main.tf
+++ main.tf
@@ -3,18 +3,11 @@ provider "aws" {
   region     = "us-east-1" # Replace with your desired region
 }
 
-terraform {
-  # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
-  # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
-  backend "s3" {
-    bucket         = "cc-tf-state-backend-ci-cd"
-    key            = "tf-infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locking"
-    encrypt        = true
-  }
-
 module "elasticdev"{
  source = "./EC2"
 }
 
+module "homechallenge" {
+  
+  source = "./homechallenege"
+}
